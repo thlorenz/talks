90c428e66575facdb559602472a26f9e6d338b02
Thorsten Lorenz <thlorenz@gmx.de>
2014-09-24 15:08:29 -0400

    improving gitbook generation

--- nodejs-addon-development/build.sh
+++ nodejs-addon-development/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 rm -rf ./gitbook
-gitbookify slides.md -o gitbook && ln -s ../img gitbook/img && gitbook serve ./gitbook
+gitbookify slides.md -o gitbook                                && \
+  rm -rf ../nodejs-addon-development-book                      && \
+  gitbook build ./gitbook -o  ../nodejs-addon-development-book && \
+  cd ../nodejs-addon-development-book                          && \
+  ln -s ../nodejs-addon-development/img/ img                   && \
+  echo "http://localhost:3000" && http-server -p 3000 -c-1

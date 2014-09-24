7f06ee977ad8a1ea1e3b348ff18096c63ef32610
Thorsten Lorenz <thlorenz@gmx.de>
2014-09-24 15:13:17 -0400

    moving book under slides dir

--- nodejs-addon-development/build.sh
+++ nodejs-addon-development/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 rm -rf ./gitbook
-gitbookify slides.md -o gitbook                                && \
-  rm -rf ../nodejs-addon-development-book                      && \
-  gitbook build ./gitbook -o  ../nodejs-addon-development-book && \
-  cd ../nodejs-addon-development-book                          && \
+gitbookify slides.md -o gitbook      && \
+  rm -rf ./book                      && \
+  gitbook build ./gitbook -o  ./book && \
+  cd ./book                          && \
   echo "http://localhost:3000" && http-server -p 3000 -c-1

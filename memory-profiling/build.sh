96abe995ec80e50f286c6ad2eb1b109fed7cc2a9
Thorsten Lorenz <thlorenz@gmx.de>
2014-11-16 09:25:45 -0600

    mp: adding generated gitbook

--- memory-profiling/build.sh
+++ memory-profiling/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+IMAGEROOT='https://raw.githubusercontent.com/thlorenz/talks/gh-pages/memory-profiling/img/'
+
 link_img() {
   for D in *; do
     if [ -d "${D}" ]; then
@@ -8,10 +10,30 @@ link_img() {
   done
 }
 
-rm -rf ./gitbook
-gitbookify slides.md -o gitbook      && \
-  rm -rf ./book                      && \
-  gitbook build ./gitbook -o  ./book && \
-  cd ./book                          && \
-  link_img                           && \
-  echo "http://localhost:3000" && http-server -p 3000 -c-1
+show() {
+  gitbookify slides.md -o prep-gitbook      && \
+    rm -rf ./book                           && \
+    gitbook build ./prep-gitbook -o  ./book && \
+    cd ./book                               && \
+    link_img                                && \
+    echo "http://localhost:3000" && http-server -p 3000 -c-1
+}
+
+build() {
+  cat slides.md                    \
+  | sed -e "s|(img/|($IMAGEROOT|g" \
+  | sed -e "s|src=\"img/|src=\"$IMAGEROOT|g" \
+  > pub-slides.md
+
+  gitbookify pub-slides.md -o prep-gitbook  && \
+    rm -rf ./book                           && \
+    gitbook build ./prep-gitbook -o  ./book && \
+    rm pub-slides.md
+}
+
+rm -rf ./prep-gitbook
+if [[ $PUBLISH == 1 ]]; then
+  build
+else
+  show
+fi

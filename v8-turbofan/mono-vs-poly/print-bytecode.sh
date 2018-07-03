b9f7e81d90d1ae0a99bcf6e192ae27bb1f9970bc
Thorsten Lorenz <thlorenz@gmx.de>
2018-07-03 11:07:41 -0600

    turbo-fan: providing paths to d8 executables via env vars

--- v8-turbofan/mono-vs-poly/print-bytecode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/Volumes/d/dev/js/sandboxes/d8box/turbo-debug/d8 \
-  --allow-natives-syntax                         \
-  --print-bytecode                               \
-  --code-comments                                \
-  --print-bytecode-filter=add*                   \
-  ./add.js
